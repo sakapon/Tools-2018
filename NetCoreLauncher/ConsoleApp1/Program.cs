d502509bd01f4f89dbb67db92bcd83e442c38a40
Keiho Sakapon <git@saka-pon.net>
2018-09-06 00:05:10 +0900

    Update code for test

--- NetCoreLauncher/ConsoleApp1/Program.cs
+++ NetCoreLauncher/ConsoleApp1/Program.cs
@@ -6,7 +6,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello ConsoleApp1!");
+            Console.WriteLine("Press [Enter] key to exit.");
+            Console.ReadLine();
         }
     }
 }
