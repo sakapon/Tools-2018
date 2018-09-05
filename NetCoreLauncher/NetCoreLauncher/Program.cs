32c03bce7cdd71de0b2c31b582d6068047763135
Keiho Sakapon <git@saka-pon.net>
2018-09-05 14:33:57 +0900

    Create project

---
+++ NetCoreLauncher/NetCoreLauncher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreLauncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
